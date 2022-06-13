92e67fd63bf8fb1bb3c108b0bfb9e4e8b27f3ac8
Luke Tonin <26802758+LukeTonin@users.noreply.github.com>
2022-06-13 11:16:05 +0100

    Attempt kedro.

--- python_setuptools/hooks/post_gen_project.py
+++ python_setuptools/hooks/post_gen_project.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 import os
+import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
-def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+def remove(path):
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, path))
 
 
-if __name__ == '__main__':
+def main():
+    if "{{ cookiecutter.add_data_directory }}" == "no":
+        remove("data")
+    if "{{ cookiecutter.add_notebooks_directory }}" == "no":
+        remove("notebooks")
+    if "{{ cookiecutter.add_conf_directory }}" == "no":
+        remove("conf")
+    if "{{ cookiecutter.add_logd_directory }}" == "no":
+        remove("logs")
 
-    if '{{ cookiecutter.license }}' == 'Proprietary':
-        remove_file('LICENSE')
+
+if __name__ == "__main__":
+    main()
